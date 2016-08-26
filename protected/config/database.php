91e52da01168cf82b257ad45e4ca48d3f7abf5dd
Leo D. Nuneza <leiyu876@yahoo.com>
2016-08-26 17:42:22 +0800

    fixed untracked files

--- protected/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-	'connectionString' => 'pgsql:host=ec2-54-235-127-48.compute-1.amazonaws.com;dbname=daphifufqkjn20',
-	'emulatePrepare' => true,
-	'username' => 'triysiubaclgcu',
-	'password' => 'uXxAwVCgKghb2beuaLkYlwpXdw',
-	'charset' => 'utf8',
-);
